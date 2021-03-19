bad0af0989767560581c323665c7e603698ed5ed
over40dev <ted@over40.dev>
2021-03-18 23:47:12 -0230

    hello-world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function HelloWorld() {
+  return <div><Hello/></div>
+}
+
+function Hello() {
+  let isHello = true;
+  return <span>
+    {isHello ? 'Hello'  : 'Goodbye' }
+  </span>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HelloWorld/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
