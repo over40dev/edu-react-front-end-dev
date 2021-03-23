20e06800c35193c66776405432a2852872a9f91f
over40dev <ted@over40.dev>
2021-03-23 18:27:00 -0230

    12-PropTypes-2

---
+++ src/static-data.js
@@ -0,0 +1,16 @@
+let items = [
+  {
+      id: 0,
+      name: "Apple iPad",
+      descriptions: "An iPad like no other.",
+      price: 329.00
+  },
+  {
+      id: 1,
+      name: "Apple iPad Pro",
+      descriptions: "Even more expensive than the iPad.",
+      price: 729.00
+  },
+];
+
+export {items};
