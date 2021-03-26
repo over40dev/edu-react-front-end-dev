e2dabe7c02ad24c688dc7598e0a85f3e7b4ba2ce
over40dev <ted@over40.dev>
2021-03-26 16:09:53 -0230

    16-Event Handling

--- src/CartPage.js
+++ src/CartPage.js
@@ -4,21 +4,32 @@ import PropTypes from 'prop-types';
 import Item from './Item';
 import './CartPage.css';
 
-// modified code (remove onAddToCart Prop)
-function CartPage({items}) {
+// modified code (add new Props)
+function CartPage({items, onAddOne, onRemoveOne}) {
   return (
     <ul className="CartPage-items">
-      {items.map(item => 
-        <li key={item.id} className="CartPage-item">
-          <Item item={item}>
-          </Item>
-        </li>)}
+      {items.map(item => <li key={item.id} className="CartPage-item">
+        <Item item={item}>
+          {/* modified code (add CartItem Controls) */}
+          <div className="CartItem-controls">
+            <button className="CartItem-removeOne" onClick={()=>onRemoveOne(item)}>
+              &ndash;
+            </button>
+            <span className="CartItem-count">{item.count}</span>
+            <button className="CartItem-addOne" onClick={()=>onAddOne(item)}>
+              +
+            </button>
+          </div>
+        </Item>
+      </li>)}
     </ul>
   );
 }
-// existing code
+// modified code (add new Props)
 CartPage.propTypes = {
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  onAddOne: PropTypes.func.isRequired,
+  onRemoveOne: PropTypes.func.isRequired
 };
 // existing code
 export default CartPage;
